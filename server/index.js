e23a489f15025cb463ecbf574d2567e433fb7e9f
cucumber6621601123 <phiriyaporn.y@ku.th>
2025-02-10 13:01:06 +0700

    postman

--- server/index.js
+++ server/index.js
@@ -1,15 +1,81 @@
-const http = require('http'); //Import Node.js core module
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-const hostname = "localhost"; //Localhost
-const port = 8000; //port number
+const port = 8000;
 
-//กำหนด listener ให้กับ server เมื่อเปิด เว็บไที่ http://localhost:8000 จะเรียกใช้ function requireListener
-const requireListener = function (req, res) {
-    res.writeHead(200);
-    res.end("My first server!"); //end the response
-}
+app.use(bodyParser.json());
 
-const server = http.createServer(requireListener); //create server
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});     
+let users = []
+let conster = 1
+
+/*
+GET /users สำหรับ get users ทั้งหมดที่บันทึกไว้
+POST /users สำหรับสร้าง users ใหม่และบันทึกเข้าไป
+GET /user/:id สำหรับดึง users รายคนออกมา ตาม id ที่ระบุ
+PUT /user/:id สำหรับแก้ไขข้อมูล users รายคน (ตาม id ที่บันทึกเข้าไป)
+DELETE /user/:id สำหรับลบ users รายคน (ตาม id ที่บันทึกเข้าไป)
+*/
+
+//path: GET /users สำหรับ get users ทั้งหมดที่บันทึกไว้
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+//path: POST /users สำหรับสร้าง users ใหม่และบันทึกเข้าไป
+app.post("/user", (req, res) => {
+    let user = req.body;
+    user.id = conster;
+    conster += 1
+    users.push(user);
+    res.json ({
+        message: "Create new user successfully",
+        user: user
+    })
+});
+
+//path: /user/:id ใช้สำหรับแสดงข้อมูล user ตาม id ที่ระบุ
+app.put("/user/:id", (req, res) => {
+    let id = req.params.id;
+    let updateUser = req.body;
+    
+    //หา users จาก id ที่ส่งมา
+    let selectedIndex = users.findIndex(user => user.id == id);
+    
+    //แก้ไขข้อมูล users ที่หาเจอ
+    if (updateUser.firstname) {
+        users[selectedIndex].firstname = updateUser.firstname;
+
+    }
+    if (updateUser.lastname) {
+        users[selectedIndex].lastname = updateUser.lastname;
+    }
+
+    res.json({
+        message: "Update user successfully",
+        data : {
+            user: updateUser,
+            indexUpdate: selectedIndex
+        }
+ 
+    })
+    //users ที่ update ใหม่ update ส่งกลับไปเก็บใน users เดิม
+
+});
+//path: DELETE /user/:id ใช้สำหรับลบ user ตาม id ที่ระบุ
+app.delete("/user/:id", (req, res) => {
+    let id = req.params.id;
+    //หา index ของ user ที่ต้องการลบ
+    let selectedIndex = users.findIndex(user => user.id == id);
+    //ลบ
+    users.splice(selectedIndex, 1);
+    res.json({
+        message: "Delete user successfully",
+        indexDeleted: selectedIndex
+    });
+
+});
+
+app.listen(port, (req, res) => {
+    console.log('HTTP Server is running on port'+ port);   
+});
